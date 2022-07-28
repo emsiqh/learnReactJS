b83b6d5f3920189c3a37645003de5b44a24a8db8
Huy <emsiqh>
2022-07-28 23:49:48 +0700

    update animation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap"
 import projImg1 from "../assets/img/prj1.png";
-import projImg2 from "../assets/img/project-img2.png";
+import projImg2 from "../assets/img/prj2.png";
 import projImg3 from "../assets/img/project-img3.png";
 
 export const Projects = () => {
@@ -10,6 +10,20 @@ export const Projects = () => {
             title: "Profile Website",
             description: "Simple web by ReactJS",
             imgUrl: projImg1,
+            tech: "Html, Css, Javascript, Reactjs",
+            gitLink: "https://github.com/emsiqh/learnReactJS.git",
+        },
+        {
+            title: "Website tin tức",
+            description: "Project bài tập lớn LTW",
+            imgUrl: projImg2,
+            tech: "JavaWeb, Html, Css, Javascript, FetchApi",
+            gitLink: "https://github.com/emsiqh/BTL-LTW.git",
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
             tech: "Html, Css, Javascript, Reactjs",
             gitLink: "https://github.com/emsiqh/learnReactJS.git",
         },
@@ -27,6 +41,13 @@ export const Projects = () => {
             tech: "Html, Css, Javascript, Reactjs",
             gitLink: "https://github.com/emsiqh/learnReactJS.git",
         },
+        {
+            title: "Profile Website",
+            description: "Simple web by ReactJS",
+            imgUrl: projImg1,
+            tech: "Html, Css, Javascript, Reactjs",
+            gitLink: "https://github.com/emsiqh/learnReactJS.git",
+        },
     ];
 
     return (
@@ -37,7 +58,7 @@ export const Projects = () => {
                         <h2>Projects</h2>
                         <p>
                             Here are a few basic projects during my studies that I've picked up on from many sources and followed.
-                            In the future there will definitely be more and higher technology :v
+                            In the future there will definitely be more and smarter technology :v
                         </p>
                     </Col>
                     <Col>
@@ -50,7 +71,7 @@ export const Projects = () => {
                                                 <img src={project.imgUrl} alt={project.title}></img>
                                                 <div className="prj-text">
                                                     <h3>{project.title}</h3>
-                                                    <span>{project.description} <br/> Using {project.tech}</span>
+                                                    <span>{project.description} <br/> Sử dụng {project.tech}</span>
                                                     <a className="git-link" href={project.gitLink}>GitHub</a>
                                                 </div>
                                             </div>
