39b82d4d1909e31071cb498ba15e0c24b2726651
Huy <emsiqh>
2022-07-29 19:53:38 +0700

    optimize code

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -20,12 +20,32 @@ export const Skills = () => {
             breakpoint: { max: 464, min: 0 },
             items: 1,
         }
-    };
+    }
 
-    const HtmlValue = 70;
-    const CssValue = 65;
-    const JsValue = 65;
-    const ReactValue = 60;
+    const skills = [
+        {
+            name: "Html",
+            value: 70
+        },
+        {
+            name: "Css",
+            value: 65
+        },
+        {
+            name: "Javascript",
+            value: 65
+        },
+        {
+            name: "ReactJS",
+            value: 50
+        },
+        {
+            name: "MySQL",
+            value: 50
+        },
+    ]
+
+
 
     return (
         <section className="skill" id="skills">
@@ -41,50 +61,28 @@ export const Skills = () => {
                                     infinite={true}
                                     className="skills-carousel"
                                 >
-                                    <div className="skill-item">
-                                        <CircularProgressbarWithChildren value={HtmlValue}>
-                                            {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                                            <div style={{ fontSize: 1.4 + 'rem'}}>
-                                                <strong>{HtmlValue}%</strong>
-                                            </div>
-                                        </CircularProgressbarWithChildren>
-                                        <h5>Html</h5>
-                                    </div>
-                                    <div className="skill-item">
-                                        <CircularProgressbarWithChildren value={CssValue}>
-                                            {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                                            <div style={{ fontSize: 1.4 + 'rem'}}>
-                                                <strong>{CssValue}%</strong>
-                                            </div>
-                                        </CircularProgressbarWithChildren>
-                                        <h5>Css</h5>
-                                    </div>
-                                    <div className="skill-item">
-                                        <CircularProgressbarWithChildren value={JsValue}>
-                                            {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                                            <div style={{ fontSize: 1.4 + 'rem'}}>
-                                                <strong>{JsValue}%</strong>
-                                            </div>
-                                        </CircularProgressbarWithChildren>
-                                        <h5>JavaScript</h5>
-                                    </div>
-                                    <div className="skill-item">
-                                        <CircularProgressbarWithChildren value={ReactValue}>
-                                            {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
-                                            <div style={{ fontSize: 1.4 + 'rem'}}>
-                                                <strong>{ReactValue}%</strong>
-                                            </div>
-                                        </CircularProgressbarWithChildren>
-                                        <h5>ReactJS</h5>
-                                    </div>
-
+                                    {
+                                        skills.map((skill, index) => {
+                                            return (
+                                                <div className="skill-item" key={index}>
+                                                    <CircularProgressbarWithChildren value={skill.value}>
+                                                        {/* Put any JSX content in here that you'd like. It'll be vertically and horizonally centered. */}
+                                                        <div style={{ fontSize: 1.4 + 'rem' }}>
+                                                            <strong>{skill.value}%</strong>
+                                                        </div>
+                                                    </CircularProgressbarWithChildren>
+                                                    <h5>{skill.name}</h5>
+                                                </div>
+                                            )
+                                        })
+                                    }
                                 </Carousel>
                             </div>
                         </div>
                     </Col>
                 </Row>
             </Container>
-            <img className="background-image-left" src={colorSharp} alt="Image" />
+            <img className="background-image-left" src={colorSharp} alt="vvv" />
         </section>
     )
 
